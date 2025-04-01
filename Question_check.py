344af6951dcd5adfc26f73edb605a1da0e661ec6
triggsx3112 <triggsx3112@masseyhigh.school.nz>
2025-04-01 13:24:39 +1300

    Update Question_check.py

--- Question_check.py
+++ Question_check.py
@@ -1,4 +1,22 @@
 
+def level_check(question):
+    while True:
 
-def question_check():
+        response = input(question).lower()
+
+
+        if response == "1" or response == "level 1":
+            return "Level 1"
+        elif response == "2" or response == "level 2":
+            return "level 2"
+        elif response == "3" or response == "level 3":
+            return "level 3"
+        elif response == "4" or response == "level 4":
+            return "level 4"
+        elif response == "5" or response == "level 5":
+            return "level 5"
+        else:
+                print("please enter a number between 1 and 5")
+
+print(level_check("what level? "))
     
