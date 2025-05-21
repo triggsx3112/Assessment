747497c8b3f8db62dbeb4ac166f28198abd341b2
triggsx3112 <triggsx3112@masseyhigh.school.nz>
2025-05-21 14:56:06 +1200

    Add files via upload

--- Quiz_quest.py
+++ Quiz_quest.py
@@ -1,3 +1,23 @@
+import random
+
+
+def string_checker(question, valid_ans=('yes', 'no')):
+
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
+    while True:
+
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            if item == user_response:
+                return item
+
+            elif user_response == item[0]:
+                return item
+
+        print(error)
+        print()
 
 
 def yes_no(question):
@@ -53,14 +73,30 @@ def int_check(question):
             print(error)
 
 
+def level_check(question):
+    while True:
+
+        response = input(question).lower()
+
+
+        if response == "1" or response == "level 1":
+            return "Level 1"
+        elif response == "2" or response == "level 2":
+            return "level 2"
+        elif response == "3" or response == "level 3":
+            return "level 3"
+        elif response == "4" or response == "level 4":
+            return "level 4"
+        else:
+                print("please enter a number between 1 and 4")
 
 
 
 
+response = "regular"
 mode = "regular"
+
 rounds_played = 0
-rounds_tied = 0
-rounds_lost = 0
 end_game = "no"
 feedback = ""
 
@@ -83,7 +119,24 @@ if num_rounds == "infinite":
     mode = "infinite"
     num_rounds = 5
 
+levels_select = level_check('''What level? 
+Level 1 is addition
+Level 2 is subtraction
+Level 3 is multiplication
+Level 4 is division
+''')
+
+
+
+
+
+    #if user_choice == "xxx":   
+
+
 while rounds_played < num_rounds:
+    #num_1 = random.randint(20, 1000)
+    #operator = random.randint(10, 1000)
+
 
     if mode == "infinite":
         rounds_heading = f"\n Round {rounds_played + 1} (Infinite Mode) "
@@ -91,28 +144,95 @@ while rounds_played < num_rounds:
     else:
         rounds_heading = f"\n round {rounds_played + 1} of {num_rounds} "
 
+
     print(rounds_heading)
 
+    if levels_select == "level 1":
+        level = "level 1"
+        levels_select = 1
+
+        num_1 = random.randint(1, 500)
+        operator = random.randint(1, 500)
+
+        ans = num_1 + operator
+        print(f"{num_1} + {operator} =")
+
+        user_choice = int_check("answer: ")
+        if user_choice == ans:
+            print("correct")
+        else:
+            print("incorrect")
+            print(f"the correct answer was {ans}")
+
+        if user_choice == "xxx":
+            break
+        else:
+            continue
 
 
 
+    elif levels_select == "level 2":
+        level = "level 2"
+        levels_select = 2
 
-    if result == "tie":
-        rounds_tied += 1
-        feedback = "its a tie"
-    elif result == "lose":
-        rounds_lost += 1
-        feedback = "You lose :( "
-    else:
-        feedback = "You won :) "
+    elif levels_select == "level 3":
+        level = "level 3"
+        levels_select = 3
 
-    round_feedback = f" {feedback}"
-    history_item = f"round: {rounds_played} - {round_feedback}"
+    elif levels_select == "level 4":
+        level = "level 4"
+        levels_select = 4
 
-    print(round_feedback)
-    game_history.append(history_item)
 
     rounds_played += 1
 
-    if mode == "infinite":
-        num_rounds += 1
+
+
+
+#print(round_feedback)
+    #game_history.append(history_item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
